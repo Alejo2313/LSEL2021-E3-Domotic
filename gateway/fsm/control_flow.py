963724b4707ae18ffcd3e10bcc003ba16eec4852
Luis Felipe <lf.velez@alumnos.upm.es>
2021-04-04 13:17:04 +0200

    Luisfe: Added http part with tests passed and added functionalities

--- gateway/fsm/control_flow.py
+++ gateway/fsm/control_flow.py
@@ -1,5 +1,8 @@
 from transitions.extensions import GraphMachine as Machine
-
+from http_client import http_client
+import sys
+import time
+import json
 class Control_flow (object):
     states = ['CONFIG','IDLE','PROCESS_DATA','PROCESS_ORDER']
     transitions=[
@@ -12,7 +15,7 @@ class Control_flow (object):
         {'trigger' : 'no_need_command', 'source' : 'PROCESS_DATA',
             'dest' : 'IDLE'},        
         {'trigger' : 'resp_ready', 'source' : 'PROCESS_DATA',
-            'dest' : 'IDLE', 'after':'send_order'},             
+            'dest' : 'IDLE','conditions' :['msg_deliver'] ,'after':'send_order'},            
         {'trigger' : 'new_order', 'source' : 'IDLE',
             'dest' : 'PROCESS_ORDER', 'after':'send_order'},                
         {'trigger' : 'need_data', 'source' : 'PROCESS_ORDER',
@@ -22,12 +25,18 @@ class Control_flow (object):
         {'trigger' : 'data_ready', 'source' : 'PROCESS_ORDER',
             'dest' : 'IDLE', 'after':'send_data'}
     ]
+
+    server_ip = "40.114.216.24"
+    header_json = {"Content-type": "application/json",
+        "Accept" : "text/plain"}
+
     def __init__(self,name):
         if name == "":
             raise ValueError("The name of the control flow must be non empty")
         self.name = name
         self.machine = Machine(model = self, states = Control_flow.states,
             initial = 'CONFIG', transitions=Control_flow.transitions)
+        self.http_con = http_client("server",Control_flow.server_ip)
 
     def send_data(self):
         print("data sent")
@@ -39,41 +48,56 @@ class Control_flow (object):
         print("order sent")
 
     def ready_to_work(self):
-        return True
+        return self.http_con.do_get("/") == 200
+    def msg_deliver(self):
+        return self.http_con.do_post("/gateData",
+            json.dumps(params),self.header_json) == 200
 
 
 
 fsm = Control_flow("GW")
-# print(fsm.state)
-# while (True):
-#     ctrl=input("Insert key:")
-#     if (ctrl=="1"):
-#         fsm.start()
-#         print(fsm.state)
-#     elif (ctrl=="2"):
-#         fsm.new_data()
-#         print(fsm.state)
-#     elif (ctrl=="3"):
-#         fsm.need_response()
-#         print(fsm.state)
-#     elif (ctrl=="4"):
-#         fsm.no_need_command()
-#         print(fsm.state)
-#     elif (ctrl=="5"):
-#         fsm.resp_ready()
-#         print(fsm.state)
-#     elif (ctrl=="6"):
-#         fsm.new_order()
-#         print(fsm.state)
-#     elif (ctrl=="7"):
-#         fsm.need_data()
-#         print(fsm.state)
-#     elif (ctrl=="8"):
-#         fsm.no_need_data()
-#         print(fsm.state)
-#     elif (ctrl=="9"):
-#         fsm.data_ready()
-#         print(fsm.state)
+print(fsm.state)
+params = {
+    "name" : "Hello",
+    "value": "World",
+    "back" : "1"
+}
+while (True):
+    ctrl=input("Insert key:")
+    if (ctrl=="1"):
+        fsm.start()
+        print(fsm.state)
+    elif (ctrl=="2"):
+        print("New data received: " + str(params))
+        fsm.new_data()
+        print(fsm.state)
+        if (params["back"] == "0"):
+            fsm.no_need_command()
+        else:
+            fsm.need_response()
+            fsm.resp_ready()
+        print(fsm.state)
+    # elif (ctrl=="3"):
+    #     fsm.need_response()
+    #     print(fsm.state)
+    # elif (ctrl=="4"):
+    #     fsm.no_need_command()
+    #     print(fsm.state)
+    # elif (ctrl=="5"):
+    #     fsm.resp_ready()
+    #     print(fsm.state)
+    elif (ctrl=="6"):
+        fsm.new_order()
+        print(fsm.state)
+    elif (ctrl=="7"):
+        fsm.need_data()
+        print(fsm.state)
+    elif (ctrl=="8"):
+        fsm.no_need_data()
+        print(fsm.state)
+    elif (ctrl=="9"):
+        fsm.data_ready()
+        print(fsm.state)
         
 # fsm.start()
-fsm.machine.get_graph().draw('control_fsm.png',prog='dot')
+#fsm.machine.get_graph().draw('control_fsm.png',prog='dot')
--- gateway/fsm/http_client.py
+++ gateway/fsm/http_client.py
@@ -3,6 +3,8 @@ import json
 
 class http_client(object):
     def __init__(self, name, server_ip):
+        if (name == "" or server_ip == ""):
+            raise ValueError("The name of name and server_ip must be non empty")
         self.name = name
         self.server_ip = server_ip
     
@@ -13,19 +15,26 @@ class http_client(object):
         return self.server_ip
     
     def do_get(self,path):
+        if (path == ""):
+            raise ValueError("Path value in do_get function must be non empty")
         conn = self.create_connection()
         conn.request("GET",path)
         r1 = conn.getresponse()
         return r1.status
     
     def do_post(self,path,params, headers):
+        if (path == ""):
+            raise ValueError("Path value in do_post function must be non empty")
+        elif (params == json.dumps({}) or headers == json.dumps({})):
+            raise ValueError("params and headers value in do_post function must be non empty")
+        
         conn = self.create_connection()
         conn.request("POST",path,params, headers)
         response = conn.getresponse()
         return response.status
 
     def create_connection(self):
-        conn = http.client.HTTPConnection(self.server_ip(),80, timeout=10)
+        conn = http.client.HTTPConnection(self.get_server_ip(),80, timeout=10)
         return conn
     
 
